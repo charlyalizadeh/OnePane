f2b4f5027824d57cd1dc6c486b889efb7dda339d
Alizadeh Charly <charly.alizadeh@axeria-iard.fr>
2025-06-05 18:05:11 +0200

    feat: add IPv4 address to the AD data import

--- src/Export-CsvADComputers.ps1
+++ src/Export-CsvADComputers.ps1
@@ -1,6 +1,6 @@
 if($PSVersionTable.PSVersion.Major -eq 5) {
     $csvStr = "Name,Enabled$([Environment]::NewLine)"
-    $computers = Get-ADComputer -Filter * | Select Name,Enabled
+    $computers = Get-ADComputer -Filter * | Select Name,Enabled, IPv4Address
     $computers | % { 
         $csvStr += "$($_.Name),$($_.Enabled)"
         if($_ -ne $computers[-1]) {
