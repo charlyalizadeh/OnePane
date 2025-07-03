af0c8b3170e362896e005d573f45a21694d40108
Alizadeh Charly <charly.alizadeh@axeria-iard.fr>
2025-07-03 10:28:25 +0200

    fix: wrong script filename

--- src/imports/import_ad.py
+++ src/imports/import_ad.py
@@ -6,7 +6,7 @@ from config import *
 def import_ad_computer():
     powershell_path = 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe'
     subprocess.call(
-        "./src/Export-CsvADComputers.ps1",
+        "./src/imports/Import-CsvADComputers.ps1",
         shell=True,
         executable=powershell_path
     )
