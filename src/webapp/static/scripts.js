a74b6b40114d376cece793b0510e4faff49ee329
Alizadeh Charly <charly.alizadeh@axeria-iard.fr>
2025-08-05 11:15:43 +0200

    refactor: move button loading code to separate functions

--- src/webapp/static/scripts.js
+++ src/webapp/static/scripts.js
@@ -1,15 +1,14 @@
 function updateTab(rows, tabId) {
     const trueDisplay = '<span style="color: green">✔</span>'
     const falseDisplay = '' //'<span style="color: red">✘</span>'
-    const table = `#${tabId}_table`;
-    let dt;
+    const table = `#${tabId}_table`
+    let dt
     // Get the DataTable
     if(!$.fn.DataTable.isDataTable(table)) {
         dt = $(table).DataTable({
             layout: {
                 topStart: ['pageLength', 'buttons']
             },
-            buttons: ['colvis'],
             lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
             pageLength: 50,
             scrollX: true,
@@ -17,11 +16,11 @@ function updateTab(rows, tabId) {
             colResize: { isEnabled: true },
             createdRow: function (row, data, dataIndex) {
                 if(tabId != "devices") { return }
-                const isValid = data[data.length - 1] == trueDisplay;
+                const isValid = data[data.length - 1] == trueDisplay
                 if (isValid) {
-                    $(row).addClass('row-valid');
+                    $(row).addClass('row-valid')
                 } else {
-                    $(row).addClass('row-invalid');
+                    $(row).addClass('row-invalid')
                 }
             },
             headerCallback: function(thead) {
@@ -33,65 +32,75 @@ function updateTab(rows, tabId) {
                     'text-align': 'left',
                     'font-weight': '600',
                     'border-radius': '0.5rem 0.5rem 0 0'
-                });
+                })
             }
         })
     }
     else {
-        dt = $(table).DataTable();
-        dt.clear();
+        dt = $(table).DataTable()
+        dt.clear()
     }
     // Setup the data
     rows.forEach((row, index) => {
-        const rowData = [index + 1, ...Object.values(row)];
+        const rowData = [index + 1, ...Object.values(row)]
         for(let i = 0; i < rowData.length; i++) {
             if(rowData[i] == true)
                 rowData[i] = trueDisplay
             if(rowData[i] == false)
                 rowData[i] = falseDisplay
         }
-        dt.row.add(rowData);
-    });
-    dt.draw();
+        dt.row.add(rowData)
+    })
+    dt.draw()
+}
+
+function setBtnStateLoad(btn, btnText) {
+    btn.disabled = true
+    btnText.innerHTML = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...`
+}
+
+function setBtnStateSuccess(btn, btnText) {
+    btnText.innerHTML = '<i class="bi bi-check-lg"></i>'
+    btn.classList.remove('btn-outline-primary')
+    btn.classList.add('btn-outline-success')
+    setTimeout(() => {
+        btnText.innerHTML = '<i class="bi bi-arrow-clockwise"></i>'
+        btn.disabled = false
+        btn.classList.remove('btn-outline-success')
+        btn.classList.add('btn-outline-primary')
+    }, 1500)
+}
+
+function setBtnStateFailure(btn, btnText) {
+    console.error("Refresh failed:", error)
+    btnText.innerHTML = `Error`
+    btn.classList.remove('btn-outline-primary')
+    btn.classList.add('btn-outline-danger')
+    setTimeout(() => {
+        btnText.textContent = "Refresh"
+        btn.disabled = false
+        btn.classList.remove('btn-outline-danger')
+        btn.classList.add('btn-outline-primary')
+    }, 1500)
 }
 
 function refreshTab(tabId, update) {
-    const btn = document.getElementById(`refresh-${tabId}`);
-    const btnText = document.getElementById(`refresh-${tabId}-text`);
+    const btn = document.getElementById(`refresh-${tabId}`)
+    const btnText = document.getElementById(`refresh-${tabId}-text`)
 
-    btn.disabled = true;
-    btnText.innerHTML = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...`;
-
+    setBtnStateLoad(btn, btnText)
     if(update) {
         fetch(`/update_devices/${tabId}`)
     }
     fetch(`/get_devices/${tabId}`)
         .then(response => response.json())
         .then(data => {
-            updateTab(data['rows'], tabId);
-
-            btnText.innerHTML = '<i class="bi bi-check-lg"></i>';
-            btn.classList.remove('btn-outline-primary')
-            btn.classList.add('btn-outline-success')
-            setTimeout(() => {
-                btnText.innerHTML = '<i class="bi bi-arrow-clockwise"></i>';
-                btn.disabled = false;
-                btn.classList.remove('btn-outline-success')
-                btn.classList.add('btn-outline-primary')
-            }, 1500);
+            updateTab(data['rows'], tabId)
+            setBtnStateSuccess(btn, btnText)
         })
         .catch(error => {
-            console.error("Refresh failed:", error);
-            btnText.innerHTML = `Error`;
-            btn.classList.remove('btn-outline-primary')
-            btn.classList.add('btn-outline-danger')
-            setTimeout(() => {
-                btnText.textContent = "Refresh";
-                btn.disabled = false;
-                btn.classList.remove('btn-outline-danger')
-                btn.classList.add('btn-outline-primary')
-            }, 1500);
-        });
+            setBtnStateFailure(btn, btnText)
+        })
 }
 
 function validityRulesEdit(input) {
@@ -101,3 +110,4 @@ function validityRulesEdit(input) {
     let request = new Request(`/set_validity_rule/${category}/${tool}/${state}`)
     fetch(request).catch(err => { console.log("Failed to update validity rules:", err)})
 }
+
